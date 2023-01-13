67fa6cdc86f1a01c92e844296b461fa750115050
EdgarG15 <edgarjhdez1501@gmail.com>
2023-01-13 13:47:31 -0600

    commit

--- lib/providers/db_provider.dart
+++ lib/providers/db_provider.dart
@@ -72,4 +72,11 @@ class DBProvider {
     final res = await db.query('Scans', where: 'tipo = ?', whereArgs: [tipo]);
     return res.isNotEmpty ? res.map((s) => ScanModel.fromJson(s)).toList() : [];
   }
+
+  Future<int> updateScan(ScanModel nuevoScan) async {
+    final db = await database;
+    final res = await db.update('Scans', nuevoScan.toJson(),
+        where: 'id = ?', whereArgs: [nuevoScan.id]);
+    return res;
+  }
 }
