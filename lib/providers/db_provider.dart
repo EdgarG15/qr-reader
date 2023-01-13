c75d69104fca491746024c2827d8eec1523cab3f
EdgarG15 <edgarjhdez1501@gmail.com>
2023-01-13 11:21:57 -0600

    commit

--- lib/providers/db_provider.dart
+++ lib/providers/db_provider.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:qr_reader/models/scan_model.dart';
+export 'package:qr_reader/models/scan_model.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite/sqlite_api.dart';
 
@@ -23,9 +25,33 @@ class DBProvider {
     final path = join(documentsDirectory.path, 'ScansDB.db');
     print(path);
 
-    return await openDatabase(
-      path,
-      version: 1,
-    );
+    return await openDatabase(path, version: 1, onCreate: ((db, version) async {
+      await db.execute('''
+        CREATE TABLE Scans(
+          id INTEGER PRIMARY KEY,
+          tipo TEXT,
+          valor TEXT
+        )
+      ''');
+    }));
+  }
+
+  Future<int> nuevoScanRaw(ScanModel nuevoScan) async {
+    final id = nuevoScan.id;
+    final tipo = nuevoScan.tipo;
+    final valor = nuevoScan.valor;
+
+    final db = await database;
+    final res = await db.rawInsert('''
+    INSERT INTO Scans(id, tipo, valor)
+      VALUES($id, '$tipo', '$valor')
+    ''');
+    return res;
+  }
+
+  Future<int> nuevoScan(ScanModel nuevoScan) async {
+    final db = await database;
+    final res = await db.insert('Scans', nuevoScan.toJson());
+    return res;
   }
 }
