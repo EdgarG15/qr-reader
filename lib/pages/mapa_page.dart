ac895c76fb6d89e3f876fc709cdbfb96c647d874
EdgarG15 <edgarjhdez1501@gmail.com>
2023-01-27 10:47:05 -0600

    ultimo commit

--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -14,6 +14,7 @@ class MapaPage extends StatefulWidget {
 class _MapaPageState extends State<MapaPage> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
+  MapType mapType = MapType.normal;
 
   @override
   Widget build(BuildContext context) {
@@ -25,16 +26,52 @@ class _MapaPageState extends State<MapaPage> {
       zoom: 17,
     );
 
+    //* Marcadores
+    Set<Marker> markers = <Marker>{};
+    markers.add(Marker(
+        markerId: const MarkerId('geo-location'), position: scan.getLatLng()));
+
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () async {
+                final GoogleMapController controller = await _controller.future;
+                controller.animateCamera(
+                  CameraUpdate.newCameraPosition(
+                    CameraPosition(
+                      target: scan.getLatLng(),
+                      zoom: 17,
+                    ),
+                  ),
+                );
+              },
+              icon: const Icon(Icons.my_location))
+        ],
         title: const Text('Mapa'),
       ),
       body: GoogleMap(
-        mapType: MapType.normal,
+        mapType: mapType,
+        markers: markers,
         initialCameraPosition: puntoInicial,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
+      ),
+      floatingActionButton: Container(
+        padding: const EdgeInsets.only(left: 25, bottom: 15),
+        alignment: Alignment.bottomLeft,
+        child: FloatingActionButton(
+          onPressed: () {
+            if (mapType == MapType.normal) {
+              mapType = MapType.satellite;
+            } else {
+              mapType == MapType.normal;
+            }
+            setState(() {});
+          },
+          child: const Icon(Icons.layers),
+        ),
       ),
     );
   }
--- lib/widgets/scan_buttom.dart
+++ lib/widgets/scan_buttom.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_reader/providers/scan_list_provider.dart';
 
@@ -10,17 +11,19 @@ class ScanButtom extends StatelessWidget {
     return FloatingActionButton(
       elevation: 10,
       onPressed: () async {
-        // String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-        //   '#3D8BEF',
-        //   'Cancelar',
-        //   false,
-        //   ScanMode.QR,
-        // );
-        const barcodeScanRes = 'https://fernando-herrera.com';
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#3D8BEF',
+          'Cancelar',
+          false,
+          ScanMode.QR,
+        );
+        //const barcodeScanRes = 'https://fernando-herrera.com';
         final scanListProvider =
+            // ignore: use_build_context_synchronously
             Provider.of<ScanListProvider>(context, listen: false);
+        // ignore: unused_local_variable
         final nuevoScan = await scanListProvider.nuevoScan(barcodeScanRes);
-        scanListProvider.nuevoScan('geo:15.33,15.66');
+        //scanListProvider.nuevoScan('geo:15.33,15.66');
       },
       child: const Icon(Icons.filter_center_focus),
     );
